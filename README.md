2c3927655fdfdbb6eba62b497fe35415ecf4fa0d
Walid Lamraoui <m4dcod3r@protonmail.com>
2024-03-30 05:37:56 +0100

    add and setup bufferline, add readme file

---
+++ README.md
@@ -0,0 +1,39 @@
+## My Collection of Dotfiles symlinked.
+
+### Installation
+
+#### 1. Setup the symlinks :
+
+First clone this repos and make sure `stow` is present on your system in order to using to create the symlinks
+
+###### Install `stow` (Debian)
+
+```bash
+sudo apt install stow
+```
+
+Then run :
+
+```bash
+stow --target ~/.config .
+```
+
+#### 2. Install the needed packages
+
+Including `neovim`, `starship`, `lazygit` ..etc
+
+main list of packages:
+
+- neovim
+- starship
+- ripgrep
+- lazygit
+- ...
+
+You can find the full list in `leaves.txt`
+
+##### Install using brew :
+
+```bash
+xargs brew install < leaves.txt
+```
---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,71 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = { "UIEnter" },
+  keys = {
+    { ",t", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+    { ",P", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    { ",o", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+    { ",r", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+    { ",l", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+    { ",p", "<Cmd>BufferLinePick<CR>", desc = "Pick buffer" },
+    { ",d", "<Cmd>lua require('mini.bufremove').delete(n, false)<CR>", desc = "Pick buffer and close" },
+    { ",w", "<Cmd>lua require('mini.bufremove').delete(n, false)<CR>", desc = "Pick buffer and close" },
+    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "<Tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+    { "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "[m", "<cmd>BufferLineMoveNext<cr>", desc = "Move to Next buffer" },
+    { "]m", "<cmd>BufferLineMovePrev<cr>", desc = "Move to Prev buffer" },
+  },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "echasnovski/mini.bufremove",
+    {
+      "tiagovla/scope.nvim",
+      event = "UIEnter",
+      lazy = true,
+      config = function()
+        require("scope").setup({ restore_state = true })
+      end,
+    },
+  },
+  opts = {
+    options = {
+      hover = {
+        enabled = true,
+        delay = 50,
+        reveal = { "close" },
+      },
+      close_command = function(n)
+        require("mini.bufremove").delete(n, false)
+      end,
+      right_mouse_command = function(n)
+        require("mini.bufremove").delete(n, false)
+      end,
+      diagnostics = "nvim_lsp",
+      numbers = function(opts)
+        return string.format("[%s]", opts.ordinal)
+      end,
+      separator_style = "thin",
+      always_show_bufferline = true,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require("NeutronVim.core.icons").diagnostics
+        local ret = (diag.error and icons.Error .. " " .. diag.error .. " " or "")
+          .. (diag.warning and icons.Warning .. " " .. diag.warning or "")
+        return vim.trim(ret)
+      end,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Explorer |  🔍",
+          highlight = "Directory",
+          text_align = "left",
+          highlight = "BufferLineHint",
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+  end,
+}
