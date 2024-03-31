96ec91b5cd886042055d35675bdfcafc237dbe00
Walid Lamraoui <m4dcod3r@protonmail.com>
2024-03-31 01:50:04 +0100

    map <leader>fo to open telescope old files (like nvchad)

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -78,7 +78,7 @@ return {
     local builtin = require("telescope.builtin") 
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
-    vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+    vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
     require("telescope").load_extension("ui-select")
